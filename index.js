a87a2146c69f3ced3799b0871487e6161509b612
Pavel-Tomilov <pasha.tomiloff@gmail.com>
2025-07-15 21:38:50 +0300

    960

--- index.js
+++ index.js
@@ -2,28 +2,25 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  const toggleMenu = document.querySelector('.burger');
-  const menu = document.querySelector('.nav');
-
-  toggleMenu.addEventListener('click', function() {
-    this.classList.toggle('active');
-    menu.classList.toggle('open');
-
-    // Блокировка прокрутки страницы
-    if (menu.classList.contains('open')) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
-  });
-
-  // Закрытие меню при клике на ссылку
-  const navLinks = document.querySelectorAll('.nav__item-link');
-  navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-      toggleMenu.classList.remove('active');
-      menu.classList.remove('open');
-      document.body.style.overflow = '';
+    const burgerBtn = document.querySelector('.burger-btn');
+    const nav = document.querySelector('.nav');
+    const header = document.querySelector('.header');
+    
+    burgerBtn.addEventListener('click', function() {
+        this.classList.toggle('active');
+        nav.classList.toggle('active');
+        header.classList.toggle('active');
+        
+        document.body.style.overflow = nav.classList.contains('active') ? 'hidden' : '';
     });
-  });
+    
+    // Закрытие при клике на ссылки
+    document.querySelectorAll('.nav__item-link').forEach(link => {
+        link.addEventListener('click', () => {
+            burgerBtn.classList.remove('active');
+            nav.classList.remove('active');
+            header.classList.remove('active');
+            document.body.style.overflow = '';
+        });
+    });
 });
