ab1f1529a58134514c69987567a3c998f121802a
Tomilov <ru14772@nmgr.nissan-mfg-ru.biz>
2025-07-15 16:07:14 +0300

    burger

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleMenu = document.querySelector('.burger');
+  const menu = document.querySelector('.nav');
+
+  toggleMenu.addEventListener('click', function() {
+    this.classList.toggle('active');
+    menu.classList.toggle('open');
+
+    // Блокировка прокрутки страницы
+    if (menu.classList.contains('open')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  });
+
+  // Закрытие меню при клике на ссылку
+  const navLinks = document.querySelectorAll('.nav__item-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      toggleMenu.classList.remove('active');
+      menu.classList.remove('open');
+      document.body.style.overflow = '';
+    });
+  });
+});
